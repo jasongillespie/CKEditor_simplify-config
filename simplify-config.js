0f27f2c9bc58473c5c43b1c2fd915143291c3a7a
Caleb Pierce <caleb.l.pierce@gmail.com>
2014-03-07 13:09:06 -0500

    Update simplify-config.js
    formatting improvements

--- simplify-config.js
+++ simplify-config.js
@@ -1,47 +1,47 @@
-CKEDITOR.on( 'dialogDefinition', function( ev )
-	{
-		// Take the dialog name and its definition from the event data.
-		var dialogName = ev.data.name;
-		var dialogDefinition = ev.data.definition;
- 
-		if (dialogName == 'image') {
+CKEDITOR.on( 'dialogDefinition', function( ev ) {
+	
+	// Take the dialog name and its definition from the event data.
+	var dialogName = ev.data.name;
+	var dialogDefinition = ev.data.definition;
 
-			dialogDefinition.removeContents('advanced');
-			dialogDefinition.removeContents('Link');
+	if (dialogName == 'image') {
 
-			// Get a reference to the 'Link Info' tab. 
-			var infoTab = dialogDefinition.getContents('info'); 
-			infoTab.remove('ratioLock'); 
-        	infoTab.remove('txtHeight');          
-        	infoTab.remove('txtWidth');          
-        	infoTab.remove('txtBorder'); 
-        	infoTab.remove('txtHSpace'); 
-        	infoTab.remove('txtVSpace'); 
-        	infoTab.remove('cmbAlign');
+		dialogDefinition.removeContents('advanced');
+		dialogDefinition.removeContents('Link');
 
-        	dialogDefinition.onLoad = function () { 
+		// Get a reference to the 'Link Info' tab. 
+		var infoTab = dialogDefinition.getContents('info'); 
+		infoTab.remove('ratioLock'); 
+		infoTab.remove('txtHeight');          
+		infoTab.remove('txtWidth');          
+		infoTab.remove('txtBorder'); 
+		infoTab.remove('txtHSpace'); 
+		infoTab.remove('txtVSpace'); 
+		infoTab.remove('cmbAlign');
+	
+		dialogDefinition.onLoad = function () { 
 	            var dialog = CKEDITOR.dialog.getCurrent(); 
 	            var elem = dialog.getContentElement('info','htmlPreview');     
 	            elem.getElement().hide(); 
 	        }; 
-		}
+	}
 
-		else if (dialogName == 'table') { 
-        	dialogDefinition.removeContents('advanced'); 
- 			var infoTab = dialogDefinition.getContents('info');
- 			infoTab.remove('txtCellPad');
- 			infoTab.remove('txtCellSpace');
- 			infoTab.remove('txtBorder');
- 			infoTab.remove('txtHeight');
- 			infoTab.remove('txtWidth');
- 			infoTab.remove('cmbAlign');
- 			infoTab.remove('txtCaption');
- 			infoTab.remove('txtSummary');
- 		} 
+	else if (dialogName == 'table') { 
+		dialogDefinition.removeContents('advanced'); 
+ 		var infoTab = dialogDefinition.getContents('info');
+ 		infoTab.remove('txtCellPad');
+ 		infoTab.remove('txtCellSpace');
+ 		infoTab.remove('txtBorder');
+ 		infoTab.remove('txtHeight');
+ 		infoTab.remove('txtWidth');
+ 		infoTab.remove('cmbAlign');
+ 		infoTab.remove('txtCaption');
+ 		infoTab.remove('txtSummary');
+ 	} 
 
- 		else if (dialogName = 'link') {
-			dialogDefinition.removeContents('advanced');
-			dialogDefinition.removeContents('target');
- 		}
- 
-	});
+ 	else if (dialogName = 'link') {
+		dialogDefinition.removeContents('advanced');
+		dialogDefinition.removeContents('target');
+ 	}
+
+});
